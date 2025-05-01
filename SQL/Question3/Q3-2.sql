4a3b9b1c4a756ab9c0de90fa7df493bce138609a
Ahmad Saud Azmi <107422506+SaudAzmi@users.noreply.github.com>
2025-05-02 00:54:23 +0200

    Create Q3-2.sql

---
+++ SQL/Question3/Q3-2.sql
@@ -0,0 +1,24 @@
+SELECT 
+    language,
+    COUNT(*) AS total_records,
+    
+    -- Count  fields are NULL or empty per record
+    SUM(
+        (`Short description` IS NULL OR `Short description` = '') +
+        (`Short description 2` IS NULL OR `Short description 2` = '') +
+        (`Long description` IS NULL OR `Long description` = '') +
+        (`Technical details` IS NULL OR `Technical details` = '')
+    
+    ) AS total_bad_fields,
+ -- Adding fields
+    ROUND(
+        SUM(
+            (`Short description` IS NULL OR `Short description` = '') +
+            (`Short description 2` IS NULL OR `Short description 2` = '') +
+            (`Long description` IS NULL OR `Long description` = '') +
+            (`Technical details` IS NULL OR `Technical details` = '')
+        ) / (COUNT(*) * 4), 2
+    ) AS relative_bad_quality 
+ FROM clean_catalog
+GROUP BY language
+ORDER BY total_bad_fields DESC;
