c49321b6d4dea05e25a4325b70fbf3a36d815c6d
Ahmad Saud Azmi <107422506+SaudAzmi@users.noreply.github.com>
2025-05-02 00:03:29 +0200

    Update Q1.sql

--- SQL/Question1/Q1.sql
+++ SQL/Question1/Q1.sql
@@ -2,7 +2,7 @@
 SELECT 
     manufacturername,
     COUNT(*) AS total_records,
-    -- Count  fields are NULL or empty per record
+    ---- Total number of bad fields (NULL or empty descriptions)
     SUM(
         (`Short description` IS NULL OR `Short description` = '') +
         (`Short description 2` IS NULL OR `Short description 2` = '') +
@@ -10,13 +10,14 @@ SELECT
         (`Technical details` IS NULL OR `Technical details` = '')
     
     ) AS total_bad_fields,
+-- Relative bad quality = bad fields / total possible fields 
     ROUND(
         SUM(
             (`Short description` IS NULL OR `Short description` = '') +
             (`Short description 2` IS NULL OR `Short description 2` = '') +
             (`Long description` IS NULL OR `Long description` = '') +
             (`Technical details` IS NULL OR `Technical details` = '')
-        ) / (COUNT(*) * 3), 2
+        ) / (COUNT(*) * 4), 2
     ) AS relative_bad_quality 
  FROM clean_catalog
 GROUP BY manufacturername
