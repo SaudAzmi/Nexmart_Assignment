6e351fd775c1151cdb4487b83545bc548c37433b
Ahmad Saud Azmi <107422506+SaudAzmi@users.noreply.github.com>
2025-05-01 23:51:11 +0200

    Update and rename scripts/question1.sql to scripts/question1/Question1.sql

---
+++ scripts/question1/Question1.sql
@@ -0,0 +1,26 @@
+/*Question 1) Which manufacturers have the biggest improvement potential in their data quality in absolute and relative numbers? */
+SELECT 
+    manufacturername,
+    COUNT(*) AS total_records,
+    -- Count  fields are NULL or empty per record
+    SUM(
+        (`Short description` IS NULL OR `Short description` = '') +
+        (`Short description 2` IS NULL OR `Short description 2` = '') +
+        (`Long description` IS NULL OR `Long description` = '') +
+        (`Technical details` IS NULL OR `Technical details` = '')
+    
+    ) AS total_bad_fields,
+    ROUND(
+        SUM(
+            (`Short description` IS NULL OR `Short description` = '') +
+            (`Short description 2` IS NULL OR `Short description 2` = '') +
+            (`Long description` IS NULL OR `Long description` = '') +
+            (`Technical details` IS NULL OR `Technical details` = '')
+        ) / (COUNT(*) * 3), 2
+    ) AS relative_bad_quality 
+ FROM clean_catalog
+GROUP BY manufacturername
+ORDER BY total_bad_fields DESC;
+
+
+
